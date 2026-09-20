9131365d4828c695940b8d9581dfda4f2dbca297
GhadgeSiddhu <siddhughadge09@gmail.com>
2026-09-20 12:44:20 +0530

    Added patient entity and database setup

---
+++ Backend/HospitalManagement.API/Data/HospitalDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HospitalManagement.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagement.API.Data
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
--- Backend/HospitalManagement.API/Program.cs
+++ Backend/HospitalManagement.API/Program.cs
@@ -1,9 +1,15 @@
+using HospitalManagement.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<HospitalDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 
 var app = builder.Build();
 
